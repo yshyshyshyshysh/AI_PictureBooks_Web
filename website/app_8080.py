cc5e7751f5d96e4d8c2d17a7d33bee25965c3b2a
yshyshyshyshysh <54cindyhu@gmail.com>
2024-06-04 18:06:30 +0800

    profile ok

--- website/app_8080.py
+++ website/app_8080.py
@@ -63,35 +63,6 @@ def creator():
     uid = data.get('uid')
     storyToken = data.get('storyToken')
 
-    data = {
-        "title": "小牛天上飛1",
-        "From_To": "Eng:Chinese",
-        "image_urls": ["https://firebasestorage.googleapis.com/v0/b/mywebsite-vivian.appspot.com/o/Rabbit%20flying%20in%20the%20sky%2Fimage_0.jpg?alt=media&token=95529af0-33be-4682-a0d6-5c5f13a2946c","https://firebasestorage.googleapis.com/v0/b/mywebsite-vivian.appspot.com/o/Rabbit%20flying%20in%20the%20sky%2Fimage_0.jpg?alt=media&token=95529af0-33be-4682-a0d6-5c5f13a2946c","https://firebasestorage.googleapis.com/v0/b/mywebsite-vivian.appspot.com/o/Rabbit%20flying%20in%20the%20sky%2Fimage_0.jpg?alt=media&token=95529af0-33be-4682-a0d6-5c5f13a2946c",
-        "https://firebasestorage.googleapis.com/v0/b/mywebsite-vivian.appspot.com/o/Rabbit%20flying%20in%20the%20sky%2Fimage_0.jpg?alt=media&token=95529af0-33be-4682-a0d6-5c5f13a2946c"
-        ],
-        "paragraphs": [
-            "一只小兔子总是夢想著飞行，所以他制作了翼膜，然後跳下地面。",
-            "兔子一飛一舞，高飞到云海上，發現了天空中的其他生物。",
-            "兔子感謝自己制作的翼膜，現在可以飞上天空。",
-            "但是他知道，有時候也要返回地面，繼續努力成長。"
-        ],
-        "translations": [
-            "A little rabbit always dreamed of flying, so he made a wing membrane and jumped off the ground.",
-            "The rabbit fluttered and flew to the sea of ​​clouds and found other creatures in the sky.",
-            "The rabbit thanks to the wing membrane he made, and now he can fly into the sky.",
-            "But he knew that sometimes he had to return to the ground and continue to work hard."
-        ],
-        "speech_urls": [
-            "https://firebasestorage.googleapis.com/v0/b/mywebsite-vivian.appspot.com/o/audio%2Fdramatic-background-music-for-short-videos-1-minute-little-alicia-155718.mp3?alt=media&token=92424fd6-1d15-4956-a21d-2e2860b38a0a",
-            "https://firebasestorage.googleapis.com/v0/b/mywebsite-vivian.appspot.com/o/audio%2Fdramatic-background-music-for-short-videos-1-minute-little-alicia-155718.mp3?alt=media&token=92424fd6-1d15-4956-a21d-2e2860b38a0a",
-            "https://firebasestorage.googleapis.com/v0/b/mywebsite-vivian.appspot.com/o/audio%2Fdramatic-background-music-for-short-videos-1-minute-little-alicia-155718.mp3?alt=media&token=92424fd6-1d15-4956-a21d-2e2860b38a0a",
-            "https://firebasestorage.googleapis.com/v0/b/mywebsite-vivian.appspot.com/o/audio%2Fdramatic-background-music-for-short-videos-1-minute-little-alicia-155718.mp3?alt=media&token=92424fd6-1d15-4956-a21d-2e2860b38a0a"
-        ],
-        "uid": uid,
-        "storyToken": storyToken
-    }
-    # return jsonify(data)
-
     title_eng = translate_to_eng(title)
     story_info = generate_story(title)
     print("...SUCCESSFULLY generated story")
@@ -112,8 +83,7 @@ def creator():
         'story': story_info,
         'translation': translation
     }
-    print("...check infomation")
-    print(story_data)
+
 
 
     json_blob = bucket.blob(f'{title_eng}/{title_eng}.json')
@@ -148,7 +118,7 @@ def creator():
     log_data = json.loads(log_blob.download_as_text()) if log_blob.exists() else []
     log_entry = {
         'title': title,
-        'From_To': language_info,
+        'language': language,
         'title_eng': title_eng,
         'story_url': json_url,
         'image_urls': image_url,
@@ -164,14 +134,14 @@ def creator():
 
     print("...check infomation")
     print(log_entry)
-    
+
     # https://firebasestorage.googleapis.com/v0/b/webapp-ecc1b.appspot.com/o/story_logs.json?alt=media&token=1560ce88-e76f-473f-a38d-d7caec27c511
 
 
     retData = {
         "title": title,
-        "From_To": "Eng:Chinese",
-        "paragraghs": paragraphs,
+        "language": language,
+        "paragraphs": paragraphs,
         "translations": story_data["translation"],
         "image_urls": image_url,
         "speech_urls": mp3_urls,
